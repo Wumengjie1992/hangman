25d6d2fa35f371dacf0837296c0f9f8c9f0ae066
Wumengjie1992 <51589673+Wumengjie1992@users.noreply.github.com>
2019-06-26 16:58:11 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 import random
 f=open('a.txt','r+')
-k=10000
+k=1000
 for num in range(1000):
     count=random.randint(5,10)
     f.write(','.join([str(i) for i in range(k,k+count)])+'\n')
